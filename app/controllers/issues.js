03d3294c51e1f1f8f1d457f7314fdf9385084c1d
Dominic <dominic.brause@emc.com>
2016-02-26 16:05:35 +0000

    - datumsfilter beta ready

--- app/controllers/issues.js
+++ app/controllers/issues.js
@@ -2,12 +2,22 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-	issues: Ember.computed(function() {
-    			return this.store.peekAll('issue');
+	search: Ember.computed('id', function() {
+    			return this.get('issues').filterBy('id');
   	}),
 
-	search: Ember.computed('issues.id', function() {
-    			return this.get('issues').filterBy('id');
-  	})
+	activity: Ember.computed('journals', function() {
+				var issueActivity = Ember.A([]),
+					records = this.get('created_on');
 
+				for(var index in records) {
+
+					issueActivity.push(index)
+					// for (var created_on of index) {
+					// 	issueActivity.push(created_on);
+					// }
+				}
+
+				return records;
+	})
 });
