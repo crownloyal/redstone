910abd45fa97736771a24f752d2a3d48343d2c3b
Dominic <dominic.brause@emc.com>
2016-02-28 14:16:45 +0000

    - added card view to projects - modified the really simple looking project view - experiment further with search

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,7 +4,7 @@ export default Ember.Route.extend({
 	model() {
 		return Ember.RSVP.hash({
 			issues: this.store.findAll('issue'),
-			project: this.store.findAll('project')
+			project: this.store.findAll('project'),
 		});
 	}
 });
