a162b0d71c47ab8663b00dfca6da77b02a7f3dee
Dominic <dominic.brause@emc.com>
2016-03-03 11:24:30 +0000

    - fixed dependencies - fixed controller setups - updated today's tickets in mirage - masonry layout for cards implemented - dashboard and project pages transferred to masonry - card template adapted to masonry

--- app/controllers/issues.js
+++ app/controllers/issues.js
@@ -2,27 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
 
-	setupController: Ember.computed(function(controller, model){
-		controller.set('content', model);
-	}),
-
-	activity: Ember.computed('content', function() {
+	activity: Ember.computed(function() {
 		var issueActivity = Ember.A([['Date', 'Comments']]),
 			tempArray = [],
-			data = this.get('content');
+			data = this.get('model');
 
 		for(var journals in data) {
-			if(journals === 'journals'){
-				issueActivity.push(Object.keys(journals).length);
-				// for(var index in journals){
-				// 	issueActivity.push(index);
-				// }
-			// 	for(var i = Object.keys(journals).length; i != 0; i--) {
+			// if(journals === 'journals'){
+			// 	issueActivity.push(Object.keys(journals).length);
+			// 	for(var index in journals){
+			// 		issueActivity.push(index);
+			// 	}
+			// 	for(var i = Object.keys(journals).length; i !== 0; i--) {
 			// 		issueActivity.push(journals[i]);
 			// 	}
-			}
+			// }
 		}
-
+		// console.log(data);
 		return issueActivity;
 	})
 });
