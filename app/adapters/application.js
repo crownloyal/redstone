dad913e5efedc1383553f318df568adf628b6553
AEROdom <Dominic.Brause@emc.com>
2016-03-21 17:17:28 +0000

    - jsonp adapter as REST extension

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,14 +3,35 @@ import DS from 'ember-data';
 export default DS.RESTAdapter.extend({
 
 	host: 'http://redmine.mozy.lab.emc.com',
-	jsonp: 'redstone',
-	dataType: 'jsonp',
 	namespace: '',
 	headers: {
 		"X-Redmine-API-Key": '3b49bc0cbd087e360052d9e225d5f5326a83cb20'
 	},
 	buildURL: function(record, suffix) {
 		return this._super(record, suffix) + '.json';
-	}
+	},
+
+	ajaxOptions(url, type, options) {
+    var hash = options || {};
+    hash.url = url;
+    hash.type = type;
+    hash.dataType = 'jsonp';
+    hash.context = this;
+
+    if (hash.data && type !== 'GET') {
+      hash.contentType = 'application/javascript; charset=utf-8';
+      hash.data = JSON.stringify(hash.data);
+    }
+
+    var headers = get(this, 'headers');
+    if (headers !== undefined) {
+      hash.beforeSend = function (xhr) {
+        Object.keys(headers).forEach((key) =>  xhr.setRequestHeader(key, headers[key]));
+      };
+    }
+
+    return hash;
+  }
+
 
 });
--- app/routes/application.js
+++ app/routes/application.js
@@ -4,7 +4,6 @@ export default Ember.Route.extend({
 	model() {
 		return Ember.RSVP.hash({
 			issues: this.store.query('issue', {
-				callback: 'redstone',
   				limit: 100,
   				offset: 0
 			}),
