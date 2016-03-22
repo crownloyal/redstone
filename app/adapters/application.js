e6d669a81b334ea66ea9084d2b1c6de48755436a
AEROdom <Dominic.Brause@emc.com>
2016-03-22 10:08:49 +0000

    - jsonp support implemented - Redmine live data available

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,9 +4,6 @@ export default DS.RESTAdapter.extend({
 
 	host: 'http://redmine.mozy.lab.emc.com',
 	namespace: '',
-	headers: {
-		"X-Redmine-API-Key": '3b49bc0cbd087e360052d9e225d5f5326a83cb20'
-	},
 	buildURL: function(record, suffix) {
 		return this._super(record, suffix) + '.json';
 	},
@@ -23,7 +20,7 @@ export default DS.RESTAdapter.extend({
       hash.data = JSON.stringify(hash.data);
     }
 
-    var headers = get(this, 'headers');
+    var headers = Ember.get(this, 'headers');
     if (headers !== undefined) {
       hash.beforeSend = function (xhr) {
         Object.keys(headers).forEach((key) =>  xhr.setRequestHeader(key, headers[key]));
